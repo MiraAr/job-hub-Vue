74b9d756f506e392b24de823fc1e74bf53aa1dbf
MiraAr <mira.ar230@gmail.com>
2025-02-17 18:12:49 +0300

    Adding job-hub application

---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {defineProps} from 'vue';
+
+defineProps({
+    bgClass: {
+        type: String,
+        default: 'bg-gray-100'
+    }
+})
+</script>
+
+<template>
+    <div :class="`${bgClass} p-6 rounded-lg shadow-md`">
+        <slot></slot>
+    </div>
+</template>
